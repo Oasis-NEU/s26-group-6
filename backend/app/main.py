aeb5d5b3975ad2e3188e7046e7a972fd319a1ac4
emanuel8006 <emanuelgalindog12@gmail.com>
2026-03-14 16:51:40 -0400

    added CORS

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,10 +1,19 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.routers import menu
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],  # Vite's default port
+    allow_credentials=True,
+    allow_methods=["GET"],
+    allow_headers=["*"],
+)
 
 app.include_router(menu.router)
 
 @app.get("/")
 def root():
-    return {"message": "Oasis dineoncampus API running"}
+    return {"message": "Oasis API running"}
